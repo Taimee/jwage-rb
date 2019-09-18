1eb88cad5f953d41b23631d37539e3f86c2d7aa2
pokohide <hyde31415@gmail.com>
2019-09-18 17:11:57 +0900

    日付を渡したらその時点の最低賃金を返す処理を追加

--- lib/jwage.rb
+++ lib/jwage.rb
@@ -1,18 +1,26 @@
 # coding: utf-8
 # frozen_string_literal: true
 
-require 'psych'
 require 'yaml'
 require 'jwage/version'
 require 'jwage/code'
+require 'jwage/minimum_wage'
 
 module Jwage
 
   module_function
 
-  def find(input)
+  def find(input = nil, target_date = nil)
+    return nil if input.nil? || target_date.nil?
+
     code = Jwage::Code.find(input)
-    p code
     return nil if code.nil?
+
+    yaml_file = DATA_DIR.join("minimum_wages/#{format('%02d', code.to_i)}.yaml")
+    return nil unless File.exist?(yaml_file)
+
+    data = YAML.load(File.read(yaml_file))
+    m_wages = data.map { |d| Jwage::MinimumWage.new(d) }
+    m_wages.find { |wage| wage.include?(target_date) }
   end
 end
---
+++ lib/jwage/minimum_wage.rb
@@ -0,0 +1,16 @@
+module Jwage
+  class MinimumWage
+    attr_reader :value, :start_at, :end_at 
+
+    def initialize(row)
+      @value = row['wage']
+      @start_at = row['start_at']
+      @end_at = row['end_at']
+    end
+
+    def include?(date)
+      # この部分、timeが日時の場合がどういう挙動か確認する
+      (@start_at <= date) && (@end_at.nil? || date <= @end_at)
+    end
+  end
+end
